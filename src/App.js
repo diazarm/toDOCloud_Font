c11572072197786e15f3ed2cd830f7991335cfa7
unknown <marceloardiaz@gmail.com>
2025-04-09 22:56:45 -0400

    feat: creando front Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await axios.get('http://localhost:5000/api/todos');
+    setTodos(response.data);
+  };
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (!text) return;
+    
+    const response = await axios.post('http://localhost:5000/api/todos', { text });
+    setTodos([response.data, ...todos]);
+    setText('');
+  };
+
+  const toggleComplete = async (id) => {
+    const todo = todos.find(todo => todo._id === id);
+    const response = await axios.patch(`http://localhost:5000/api/todos/${id}`, {
+      completed: !todo.completed
+    });
+    
+    setTodos(todos.map(t => t._id === id ? response.data : t));
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`http://localhost:5000/api/todos/${id}`);
+    setTodos(todos.filter(todo => todo._id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      
+      <div className="todo-list">
+        {todos.map(todo => (
+          <div key={todo._id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo._id)}
+            />
+            <span>{todo.text}</span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
